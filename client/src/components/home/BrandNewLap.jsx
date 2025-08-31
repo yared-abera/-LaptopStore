31271b2eda0f3f967f085e1a69d31ab0a5306ff5
yared-abera <yaredoabera7@gmail.com>
2025-08-31 12:22:57 +0300

    loading spinner added to brandNewLap comp

--- client/src/components/home/BrandNewLap.jsx
+++ client/src/components/home/BrandNewLap.jsx
@@ -3,42 +3,52 @@ import UserProductCardView from '../shopping-view/Product-Card';
 import { useDispatch,useSelector} from 'react-redux';
 import { useEffect,useState } from 'react';
 import { FetchAllLaptops} from '../../store/laptop-slice/index';
+import { Loader2 } from "lucide-react";
+
 
 
 const BrandNewLap = () => {
 const dispatch=useDispatch();
 const {LaptopsList} = useSelector((state) => state.adminLaptops);
 const [brandNewLaptops, setBrandNewLaptops] = useState([]);
+const [isLoading, setIsLoading] = useState(true);
 
  useEffect(() => {
-   dispatch(FetchAllLaptops()).then((response) => {
+   setIsLoading(true);
+    dispatch(FetchAllLaptops()).then((response) => {
      if (response.payload && response.payload.success) {
       // console.log("Laptops fetched successfully:", response.payload);
        const brandNewLaptops=LaptopsList.filter((laptop) => laptop.condition === "brand_new");
        setBrandNewLaptops(brandNewLaptops); 
+        setIsLoading(false);
      } else {
        console.error("Failed to fetch laptops:", response.payload.message);
+       setIsLoading(false);
      }
    });
   
   },[dispatch]);
 
-
-
+  
+    
   return (
      <>
      <div className='flex gap-1 '>
       <h1 className='lg:text-4xl md:text-3xl text-2xl text-purple-600 w-[20%] font-bold 
       mb-4 my-5 pl-5 text-center '>Brand New <br/>Laptops</h1>  
        
-      {  brandNewLaptops&& brandNewLaptops.length > 0 ?
+      { brandNewLaptops&& brandNewLaptops.length > 0 ?
+      isLoading ? (<div className="flex items-center justify-center w-[80%] p-5 bg-gray-100 rounded-lg shadow-lg">
+        <Loader2 className="w-6 h-6 animate-spin text-center   text-yellow-400" />
+        <p className="ml-2 text-lg text-gray-600">Loading...</p></div>):
       (<div className='grid grid-cols-1 md:grid-cols-2
         lg:grid-cols-4 gap-4 p-5 bg-gray-100 rounded-lg shadow-lg  
-         w-[80%]'  >
+         w-[80%]'  >    
        {LaptopsList.map((laptop) => {
        return  <UserProductCardView key={laptop._id} laptop={laptop} /> 
-})}
-      </div>)
+   })}
+      </div>
+    )
       : 
       <p className='w-[80%] text-center text-2xl p-5 bg-gray-100 rounded-lg shadow-lg'>
         No brand new laptops available at the moment.
--- client/src/components/shopping-view/Product-Card.jsx
+++ client/src/components/shopping-view/Product-Card.jsx
@@ -8,22 +8,23 @@ import { Button } from '../ui/button'
 
 const UserProductCardView = ({laptop}) => {
   return (
-    <Card className='hover:shadow-lg transition-shadow duration-300 ease-in-out'>
-       <CardHeader>  
-        <CardDescription className='sr-only'>Product short description</CardDescription>
-        </CardHeader>
+    <Card className='hover:shadow-lg transition-shadow duration-300 ease-in-out '>
+        
         <CardContent className=' w-full '>  
-        <div className=' items-center justify-center align-center rounded-md w-full '>  
-        <div className='flex flex-col'>
-        <img className='h-40 w-40 transition-transform 
+        <div className=' items-center justify-center align-center rounded-md w-full border-2 border-gray-200 '>  
+        <div className='flex flex-col  w-full'>
+        <img className=' pt-2 h-40 w-40 transition-transform 
         duration-300 hover:scale-105' 
-        src={ laptop.imageUrl} alt="laptop image"
+        src={ laptop.imageUrl} alt={`Image of ${laptop.model}`} 
          />
+        <div className=' '>
         <h1> <span className='font-bold text-lg '> {laptop.model} </span></h1>
-        <h1> Price : <span className='text-red-600 hover:text-yellow-700 hover:underline'>Price ETB </span></h1>
-        </div>  
+        <h1> <span className='text-red-600 hover:text-yellow-700 hover:underline'> {laptop.price} ETB </span></h1>
+        <h1> <span className='text-gray-600 '> {laptop.condition} </span></h1>
         <div className='flex  justify-between  '>
         <Button className='mt-4  bg-yellow-700' >Add to Cart</Button> 
+        </div>
+        </div>
         </div>
       </div>
       </CardContent> 
